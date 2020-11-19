3c2631b512cbfd76a9cf318977f8a5c2138c4745
nilayingle <nilayingle18@gmail.com>
2020-11-19 08:17:50 +0530

    Created Auth API

---
+++ src/main/java/com/backend/studentmanagement/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.backend.studentmanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home()
+    {
+        return "Welcome to spring boot";
+    }
+
+}
